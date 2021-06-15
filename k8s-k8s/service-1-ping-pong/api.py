ba35a84a6c8c580417377fedc2ab89155338e43e
umair.naushad <umair.naushad@emaratech.ae>
2021-06-15 09:18:45 +0400

    externalize port/endpoint settings

--- k8s-k8s/service-1-ping-pong/api.py
+++ k8s-k8s/service-1-ping-pong/api.py
@@ -1,38 +1,36 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import requests,json
+import requests,json, os
 
 app = Flask(__name__)
 api = Api(app)
 
-url = 'http://service-2-ping-pong-svc:5001/pong'
+SERVICE_1_PORT = os.getenv('SERVICE_1_PORT')
+SERVICE_2_URL = os.getenv('SERVICE_2_URL') #'http://service-2-ping-pong-svc:5001/pong'
 
 @app.route('/')
 def welcome():
-    return "welcome to the service-1-ping-pong"
-
-@app.route('/about', methods=['GET'])
-def home():
     return {
-       'revision': '1.0.0'
-       }
+        'message': 'welcome to the service-1-ping-pong',
+        'endpoint': '/'
+    }
 
 @app.route('/ping', methods=['GET'])
 def ping():
     if request.method == 'GET':
-        data = {'service': 'k8s'}
+        data = {'service': 'k8s-service-1'}
         response = requests.post(
-            url, data=json.dumps(data),
+            SERVICE_2_URL+"pong", data=json.dumps(data),
             headers={'Content-Type': 'application/json'}
         )
-        return response.content
+    return response.content
 
 @app.route("/pong", methods=['POST'])
 def pong():
    return {
-       'app': 'k8s service ping pong',
+       'app': 'k8s service 1 ping pong',
        'endpoint': 'pong'
-       }
+    }
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 5000, debug = True)
+    app.run(host ='0.0.0.0', port = SERVICE_1_PORT, debug = True)
--- k8s-k8s/service-2-ping-pong/api.py
+++ k8s-k8s/service-2-ping-pong/api.py
@@ -1,38 +1,36 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import requests,json
+import requests, json, os
 
 app = Flask(__name__)
 api = Api(app)
 
-url = 'http://service-1-ping-pong-svc:5000/pong'
+SERVICE_2_PORT = os.getenv('SERVICE_2_PORT')
+SERVICE_1_URL = os.getenv('SERVICE_1_URL') #'http://service-1-ping-pong-svc:5000/pong'
 
 @app.route('/')
 def welcome():
-    return "welcome to the service-2-ping-pong"
-
-@app.route('/about', methods=['GET'])
-def home():
     return {
-       'revision': '1.0.0'
-       }
+        'message': 'welcome to the service-2-ping-pong',
+        'endpoint': '/'
+    }
 
 @app.route('/ping', methods=['GET'])
 def ping():
     if request.method == 'GET':
         data = {'service': 'lambda'}
         response = requests.post(
-            url, data=json.dumps(data),
+            SERVICE_1_URL+"pong", data=json.dumps(data),
             headers={'Content-Type': 'application/json'}
         )
-        return response.content
+    return response.content
 
 @app.route("/pong", methods=['POST'])
 def pong():
    return {
        'app': 'lambda ping pong',
        'endpoint': 'pong'
-       }
+    }
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 5001, debug = True)
+    app.run(host ='0.0.0.0', port = SERVICE_2_PORT, debug = True)
