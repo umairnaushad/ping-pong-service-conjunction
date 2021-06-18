daebbe7b9feba3a960b0cd56f85d29554f726e5d
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-19 00:10:40 +0400

    working on lamda

--- k8s-lambda/lambda-ping-pong/ping_pong/app.py
+++ k8s-lambda/lambda-ping-pong/ping_pong/app.py
@@ -3,19 +3,19 @@ import json, os, requests
 SERVICE_URL = os.getenv('SERVICE_URL')
 
 def lambda_handler_ping(event, context):
-    print("SERVICE_URL="+SERVICE_URL)
     data = {'service': 'lambda-service'}
+    
+    print("#############################################")
+    print("SERVICE_URL="+SERVICE_URL)
     response = requests.post(
         SERVICE_URL+"pong", data=json.dumps(data),
         headers={'Content-Type': 'application/json'}
     )
-    return response.content
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'app': 'lambda ping pong',
-            'endpoint': '/ping'
-        }),
+        "statusCode" : 200,
+        "body" : json.dumps(
+            {"message" : response.content}
+        )
     }
 
 def lambda_handler_pong(event, context):
