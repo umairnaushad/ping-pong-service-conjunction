acf5f7c98ebd693426b772e468e170c5d5309ddf
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-19 07:56:31 +0400

    Correcting path in build

--- k8s-k8s/deploy.sh
+++ k8s-k8s/deploy.sh
@@ -3,5 +3,4 @@
 echo "####################################################################"
 echo "####################### Deploy on Kubernetes #######################"
 echo "####################################################################"
-cd k8s-k8s
 kubectl apply -f service-1-ping-pong/k8s-service-1.yaml,service-2-ping-pong/k8s-service-2.yaml
