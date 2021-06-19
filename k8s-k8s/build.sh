fb81e234ef968005cd2bc901a0d3c0a731452a2f
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-19 07:56:11 +0400

    Correcting path in build

--- k8s-k8s/build.sh
+++ k8s-k8s/build.sh
@@ -3,7 +3,6 @@
 echo "####################################################################"
 echo "######################## Building Service 1 ########################"
 echo "####################################################################"
-cd k8s-k8s
 cd service-1-ping-pong
 docker build -t umairnaushad/service-1-ping-pong:1.0.1 .
 docker push umairnaushad/service-1-ping-pong:1.0.1
