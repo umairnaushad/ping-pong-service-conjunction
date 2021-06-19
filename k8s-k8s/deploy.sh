336030c83ebac7f25da8fc8759a582ceabdf9b42
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-19 08:01:59 +0400

    Adding status in deploy script

--- k8s-k8s/deploy.sh
+++ k8s-k8s/deploy.sh
@@ -4,3 +4,6 @@ echo "####################################################################"
 echo "####################### Deploy on Kubernetes #######################"
 echo "####################################################################"
 kubectl apply -f service-1-ping-pong/k8s-service-1.yaml,service-2-ping-pong/k8s-service-2.yaml
+echo "Deploying please wait ..."
+sleep 20
+kubectl get po -n devops-ping-pong
