7e8d22a87d2ee86de759b0342ef676bb474e3776
root <root@master-01>
2021-06-19 04:10:51 +0000

    changing file permissions

--- k8s-k8s/test.sh
+++ k8s-k8s/test.sh
@@ -1,6 +1,6 @@
-curl http://127.0.0.1:5001/
-curl http://127.0.0.1:5001/pong -X POST
-curl http://127.0.0.1:5001/ping
-curl http://127.0.0.1:5002/
-curl http://127.0.0.1:5002/pong -X POST
-curl http://127.0.0.1:5002/ping
+curl http://127.0.0.1:32101/
+curl http://127.0.0.1:32101/pong -X POST
+curl http://127.0.0.1:32101/ping
+curl http://127.0.0.1:32102/
+curl http://127.0.0.1:32102/pong -X POST
+curl http://127.0.0.1:32102/ping
--- k8s-lambda/build.sh
+++ k8s-lambda/build.sh
@@ -3,7 +3,7 @@
 echo "####################################################################"
 echo "########################### Building k8s ###########################"
 echo "####################################################################"
-cd k8s-lambda/k8s-ping-pong
+cd k8s-ping-pong
 docker build -t umairnaushad/k8s-ping-pong:1.0.1 .
 docker push umairnaushad/k8s-ping-pong:1.0.1
 cd ..
