cad2f2806a3ee5d24f4c7cb6637348616f4515dd
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-19 07:54:04 +0400

    Adding testing file

---
+++ k8s-k8s/test.sh
@@ -0,0 +1,6 @@
+curl http://127.0.0.1:5001/
+curl http://127.0.0.1:5001/pong -X POST
+curl http://127.0.0.1:5001/ping
+curl http://127.0.0.1:5002/
+curl http://127.0.0.1:5002/pong -X POST
+curl http://127.0.0.1:5002/ping
